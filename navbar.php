37f7ec3e760664346cb3fcd9aa702273ee357e07
Migue <migueestepa@gmail.com>
2024-10-02 19:08:00 +0200

    implementación del offcanvas

--- navbar.php
+++ navbar.php
@@ -1,30 +1,36 @@
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light py-5 ">
-<div class="container d-flex justify-content-between">
-        <!-- Centrado del logo con ms-auto -->
-        <a class="navbar-brand " href="#">FibraOlé</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-lg-center w-100" id="navbarNav">
-                <!-- w-100 se asegura que el menú ocupe todo el ancho disponible -->
-            <ul class="navbar-nav ms-lg-auto text-end w-100">
-                <!-- ms-lg-auto para centrar el menú en pantallas grandes, text-end para que las opciones se centren a la derecha -->
-                <li class="nav-item px-4">
-                    <a class="nav-link fw-bold" href="Fibra.php">Fibra</a>
-                </li>
-                <li class="nav-item px-4">
-                    <a class="nav-link fw-bold" href="packahorro.php">Packs Ahorro</a>
-                </li>
-                <li class="nav-item px-4">
-                    <a class="nav-link fw-bold" href="telefoniatv.php">Telefonía+TV</a>
-                </li>
-                <li class="nav-item px-4">
-                    <a class="nav-link fw-bold" href="serviciotecnico.php">Servicio Técnico</a>
-                </li>
-            </ul>
-        </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light py-5">
+  <div class="container d-flex justify-content-between">
+    <!-- Centrado del logo con ms-auto -->
+    <a class="navbar-brand" href="#">FibraOlé</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+        <!--Cambio del botón a offcanvas -->
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menú</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <!-- Menú de navegación -->
+        <ul class="navbar-nav ms-lg-auto w-100">
+          <li class="nav-item px-4">
+            <a class="nav-link fw-bold" href="Fibra.php">Fibra</a>
+          </li>
+          <li class="nav-item px-4">
+            <a class="nav-link fw-bold" href="packahorro.php">Packs Ahorro</a>
+          </li>
+          <li class="nav-item px-4">
+            <a class="nav-link fw-bold" href="telefoniatv.php">Telefonía+TV</a>
+          </li>
+          <li class="nav-item px-4">
+            <a class="nav-link fw-bold" href="serviciotecnico.php">Servicio Técnico</a>
+          </li>
+        </ul>
+      </div>
     </div>
+
+  </div>
 </nav>
 
 
