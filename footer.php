c3f548827d7ec071abeaa54d85674caf795d8167
insytec-alcala <soporte@einsytec.es>
2024-10-15 19:36:44 +0200

    Cambios en el footer

--- footer.php
+++ footer.php
@@ -11,13 +11,13 @@
 
         <div class="row">
             <!-- Columna info empresa -->
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <div class="row">
                     <!--Flexbox logo y telefono -->
-                    <div class="col-md-12 d-flex align-items-center">
+                    <div class="col-md-12 d-flex align-items-center my-3">
                         <!-- Imagen del móvil alineada a la izquierda -->
                         <img src="./img/fibraole/icono_movil02.png" class="me-3" style="max-height: 50px;">
-                        
+
                         <!-- Texto alineado justo a la derecha de la imagen -->
                         <div>
                             <p class="mb-0 fs-6"><small>Llama gratis al</small></p>
@@ -25,47 +25,61 @@
                         </div>
                     </div>
                 </div>
-                <p>
+
+                <div class="row">
+                    <div class="col-md-12 d-flex align-items-center">
+                        <img src="./img/fibraole/icono_incidencias.png" class="me-3" style="max-width: 50px;">
+
+                        <div>
+                            <!-- Asegurar que la frase no se divida -->
+                            <p class="mb-0 fs-6 text-nowrap">
+                                <small>Teléfono de incidencias</small>
+                            </p>
+                            <!-- Número de teléfono abajo -->
+                            <strong class="fs-4">694 498 620</strong>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <p class="my-3">
                     Síguenos en: <br>
                     <img src="./img/fibraole/icono_facebook.png">
                     <img src="./img/fibraole/icono_instagram.png">
-                    <a href="#" class="text-white">@insytecalcalareal</a>
+                    <a href="#" class="text-white">fibraole</a>
                 </p>
-                <p>Teléfono de incidencias: <br> 
-                <img src="./img/fibraole/icono_exclamacion.png">
-                     694 498 620
-                </p>
+
             </div>
             <!--Columna medio -->
-            <div class="col-md-4">
-                <ul class="list-unstyled lineavertical">
-                    <li><a href="#" class="text-white">Servicio Técnico</a></li>
-                    <li><a href="#" class="text-white">Zapi TV</a></li>
-                    <li><a href="#" class="text-white">Tarifas</a></li>
+            <div class="col-md-3 px-0">
+                <ul class="list-unstyled  border-start border-2 border-white ">
+                    <li class="m-3"><a href="#" class="text-white">Servicio Técnico</a></li>
+                    <li class="m-3"><a href="#" class="text-white">Zapi TV</a></li>
+                    <li class="m-3"><a href="#" class="text-white">Tarifas</a></li>
                 </ul>
             </div>
             <!--Columna derecha -->
-            <div class="col-md-4 border-left">
-                <ul class="list-unstyled lineavertical">
-                    <li><a href="#" class="text-white">Nosotros</a></li>
-                    <li><a href="#" class="text-white">Blog</a></li>
-                    <li><a href="#" class="text-white">FAQ</a></li>
+            <div class="col-md-3 px-0 ">
+                <ul class="list-unstyled  border-start border-2 border-white ">
+                    <li class="m-3"><a href="#" class="text-white">Nosotros</a></li>
+                    <li class="m-3"><a href="#" class="text-white">Blog</a></li>
+                    <li class="m-3"><a href="#" class="text-white">FAQ</a></li>
                 </ul>
             </div>
-        </div>
-        
-        <!-- Rollo legal -->
-        <div class="row mt-4">
-            <div class="col text-center">
-                <ul class="list-inline">
-                    <li class="list-inline-item"><a href="#" class="text-white">Avisos Legales</a></li>
-                    <li class="list-inline-item"><a href="#" class="text-white">Política de Privacidad</a></li>
-                    <li class="list-inline-item"><a href="#" class="text-white">Política de Cookies</a></li>
-                    <li class="list-inline-item"><a href="#" class="text-white">Agencia de Publicidad</a></li>
-                    <li class="list-inline-item"><a href="#" class="text-white">Agencia SEO</a></li>
-                    <li class="list-inline-item"><a href="#" class="text-white">Dobuss</a></li>
+
+            <div class="col-md-3 px-0 d-flex flex-column justify-content-end w-auto ">
+
+                <ul class="list-unstyled text-start border-end border-2 border-white ">
+                    <li class="mb-2 me-3 small-text-footer"><a href="#" class="text-white">AVISO LEGAL</a></li>
+                    <li class="mb-2 me-3 small-text-footer"><a href="#" class="text-white">POLITICA DE COOKIES</a></li>
+                    <li class="mb-2 me-3 small-text-footer"><a href="#" class="text-white">POLITICA DE PRIVACIDAD</a></li>
+
                 </ul>
+
             </div>
         </div>
+
+
     </div>
 </footer>
