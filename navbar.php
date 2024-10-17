3e76e3946ca72b186e0196c9dd39ef954e1fe94d
Migue <migueestepa@gmail.com>
2024-10-17 19:32:45 +0200

    cambios en el navbar fijo

--- navbar.php
+++ navbar.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="styles/navbar.css">
-<nav class="navbar navbar-expand-md navbar-light bg-light py-5">
+<nav class="navbar navbar-expand-md navbar-light bg-light py-3 sticky-top">
   <div class="container-fluid d-flex align-items-center justify-content-between flex-nowrap">
     <!-- Logo -->
     <a class="navbar-brand ms-5 me-5 flex-shrink-0" href="#">
@@ -16,30 +16,41 @@
   <!--offcanvas-->
   <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
     <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menú</h5>
+    <a class="navbar-brand ms-2" href="#">
+          <img src="./img/fibraole/logo_cabecera.png" alt="Logo" class="img-fluid" style="max-height: 40px;">
+        </a>
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+   
     </div>
     <div class="offcanvas-body">
       <!-- Menú de navegación -->
-      <ul class="navbar-nav mx-auto justify-content-between ms-md-auto ms-lg-0">
-      <li class="nav-item d-flex align-items-center me-2">
-          <a class="nav-link fw-bold text-dark text-nowrap" href="Fibra.php"><img class="logo-nav me-2" src="img/fibraole/icono_wifi.png "> Fibra</a>
-        </li>
-        <li class="nav-item d-flex align-items-center me-2">
-          <a class="nav-link fw-bold text-dark text-nowrap" href="packahorro.php"><img class="logo-nav me-2" src="img/fibraole/icono_ahorro.png"> Packs Ahorro</a>
-        </li>
-        <li class="nav-item d-flex align-items-center me-2">
-          <a class="nav-link fw-bold text-dark text-nowrap" href="telefoniatv.php"><img class="logo-nav me-2" src="img/fibraole/icono_movil+tv.png">Telefonía+TV</a>
-        </li>
-        <li class="nav-item d-flex align-items-center me-2">
-          <a class="nav-link fw-bold text-dark text-nowrap" href="serviciotecnico.php"><img class="logo-nav me-2" src="img/fibraole/icono_tecnico.png"> Servicio Técnico</a>
-        </li>
-        
+          <ul class="navbar-nav mx-auto justify-content-center justify-content-md-end justify-content-xl-start">
+            <li class="nav-item d-flex align-items-center me-2">
+              <a class="nav-link fw-bold text-dark text-nowrap" href="Fibra.php">
+                <img class="logo-nav me-2" src="img/fibraole/icono_wifi.png"> Fibra
+              </a>
+            </li>
+            <li class="nav-item d-flex align-items-center me-2">
+              <a class="nav-link fw-bold text-dark text-nowrap" href="packahorro.php">
+                <img class="logo-nav me-2" src="img/fibraole/icono_ahorro.png"> Packs Ahorro
+              </a>
+            </li>
+            <li class="nav-item d-flex align-items-center me-2">
+              <a class="nav-link fw-bold text-dark text-nowrap" href="telefoniatv.php">
+                <img class="logo-nav me-2" src="img/fibraole/icono_movil+tv.png"> Telefonía+TV
+              </a>
+            </li>
+            <li class="nav-item d-flex align-items-center me-2">
+              <a class="nav-link fw-bold text-dark text-nowrap" href="serviciotecnico.php">
+                <img class="logo-nav me-2" src="img/fibraole/icono_tecnico.png"> Servicio Técnico
+              </a>
+            </li>
     </ul>
-            <div class="llama-offcanvas mt-4">
-          <span class="fw-bold fs-5 text-warning">Llama gratis: 694 498 620</span>
-        </div>
-      <span class="fw-bold llama-izquierda fs-5 text-warning d-none d-lg-inline-block">Llama gratis: 694 498 620</span>
+          <div class="llama-offcanvas mt-4 text-end">
+        <span class="fw-bold fs-4 text-warning">Llama gratis:<br>
+         694 498 620</span>
+          </div>
+      <span class="fw-bold llama-izquierda fs-5 text-warning d-none d-xl-inline-block me-xl-3">Llama gratis: 694 498 620</span>
 
     </div>
   </div>
