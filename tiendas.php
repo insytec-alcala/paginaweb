6a379feb9c18f603b2d6f02763431fc0eddec14e
ter-tus <161013573+ter-tus@users.noreply.github.com>
2024-10-22 20:48:02 +0200

    Add files via upload

---
+++ tiendas.php
@@ -0,0 +1,34 @@
+<p class="fs-3 text-center fw-bolder">PUEDES CONTRATAR NUESTROS SERVICIOS EN NUESTRAS <span class="text-warning">TIENDAS FÍSICAS</span></p>
+
+    <section class="container my-5">
+    <div class="row justify-content-center text-center px-3">
+        <div class="col-md-3 ">
+            <img src="img/fibraole/icono_ubicacion.png" alt="ubicacion_castillo">
+            <p class="fw-bold fs-5 mb-0">CASTILLO DE<br>
+             LOCUBÍN</p>
+            <p fs-5>C/Veracruz 32<br>
+            681 671 156</p>
+        </div>
+        <div class="col-md-3 ">
+            <img src="img/fibraole/icono_ubicacion.png" alt="ubicacion_almedinilla">
+            <p class="fw-bold fs-5 mb-0">ALMEDINILLA</p>
+            <p fs-5>C/Cerrillo 17<br>
+            953 584 344<br>
+            694 498 620</p>
+        </div>
+        <div class="col-md-3 ">
+            <img src="img/fibraole/icono_ubicacion.png" alt="ubicacion_alcala">
+            <p class="fw-bold fs-5 mb-0">ALCALÁ LA REAL</p>
+            <p fs-5>Avda. Europa 2<br>
+            953 584 344<br>
+            694 498 620</p>
+        </div>
+        <div class="col-md-3 ">
+            <img src="img/fibraole/icono_ubicacion.png" alt="ubicacion_fuensanta">
+            <p class="fw-bold fs-5 mb-0">FUENSANTA DE<br>
+             MARTOS</p>
+            <p fs-5>C/Real 32<br>
+            953 059 797</p>
+        </div>
+    </div>
+</section>
